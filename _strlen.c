b5a5ec0c8199542f9b98c0b5629b0b8e61e91b19
Basma-Magdy <BasmaMagdy7@gmail.com>
2023-05-30 14:29:24 -0700

    string length function code

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strlen - string length
+* @s: input string
+* Return:: integer length
+*/
+int _strlen(char *s)
+{
+int i, len;
+len = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	len ++;
+}
+
+return (len);
+}
